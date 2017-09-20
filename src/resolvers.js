69d8e7786bc5b0a0e99c66e7264b775ad1a886cf
bmorlac <bmorlac@users.noreply.github.com>
2017-09-20 14:03:42 +0200

    changed query "allMovies" to fetch data from the database

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,15 +2,7 @@ import _ from 'lodash';
 
 const resolvers = {
   Query: {
-    allMovies: () => [
-      {
-        id: 1,
-        name: 'test',
-        releasedAt: new Date(),
-        addedAt: new Date(),
-        format: 'DVD',
-      },
-    ],
+    allMovies: (obj, args, { models }) => models.Movie.findAll(),
     movie: (obj, { id }, { models }) => {
       const movie = models.Movie.findOne({ where: { id } });
       return movie;
